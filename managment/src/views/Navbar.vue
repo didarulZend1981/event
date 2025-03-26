516e535f6454f828ce20b51624e6de83edadcaec
mohammad didarul islam <didarul1981@gmail.com>
2025-03-27 03:35:55 +0600

    add login & registration

--- managment/src/views/Navbar.vue
+++ managment/src/views/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-    <h1>Nevbar</h1>
+    <h1>Nevbar <router-link class="me-3 py-2 link-body-emphasis text-decoration-none" to="/login">Login</router-link>
+        <router-link class="me-3 py-2 link-body-emphasis text-decoration-none" to="/registration">registration</router-link> </h1>
 </template>
 
 <script setup>
